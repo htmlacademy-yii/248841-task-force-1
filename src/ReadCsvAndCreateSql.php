e8c810c06b788103f558b7b60039ca5d55fc2f16
Vladimir <vova_lobochkin@mail.ru>
2021-01-04 15:22:45 +0300

    Исправил конвертер csv

--- src/ReadCsvAndCreateSql.php
+++ src/ReadCsvAndCreateSql.php
@@ -8,23 +8,28 @@ use SplFileObject;
 
 class ReadCsvAndCreateSql
 {
-    protected $fileName;
-    const PATH = '/data/';
-    protected $dirName;
-    const NAME_BD = 'taskforce';
+    private $fileName;
+    private $dirName;
+    private $bdName;
+    private $outDirName;
 
     /**
      * ReadCsvAndCreateSql constructor.
      * @param string $name
+     * @param string $dirName
+     * @param string $outDirName
+     * @param string $bdName
      */
 
-    function __construct(string $name)
+    function __construct(string $name, string $dirName, string $outDirName, string $bdName)
     {
-        $this->fileName = $name;
-        $this->dirName = $_SERVER['DOCUMENT_ROOT'] . self::PATH;
+        $this->fileName = str_replace(".csv", '', $name);
+        $this->dirName = $dirName;
+        $this->bdName = $bdName;
+        $this->outDirName = $outDirName;
     }
 
-    public function convertStringCsv(): string
+    public function csvToSqlConvert(): string
     {
         $fullName = $this->dirName . $this->fileName . ".csv";
 
@@ -33,29 +38,36 @@ class ReadCsvAndCreateSql
         }
 
         $file = new SplFileObject($fullName);
+        if ($file->getExtension() !== 'csv') {
+            throw new \Exception("Файл {$fullName} имеет расширение {$file->getExtension()}");
+        }
+
+        if (!$file->getSize()) {
+            throw new \Exception("Файл {$fullName} пустой");
+        }
+
         $file->setFlags(SplFileObject::READ_CSV);
         $headers = '';
-        $sqlQuestion = "INSERT INTO " . self::NAME_BD . ".{$this->fileName} (";
+        $sqlQuestion = "INSERT INTO " . $this->bdName . ".{$this->fileName} (`";
         foreach ($file as $row) {
             if (!$headers) {
-                $headers = implode($row, ',');//VALUES
-                $sqlQuestion .= $headers . ") VALUES";
+                $headers = implode($row, '`,`');//VALUES
+                $sqlQuestion .= $headers . "`) VALUES";
             } else {
-                if (count(array_filter($row))>0) {
+                if (trim($row[0]) !== '') {
 
-                    $sqlQuestion .= '("' . implode($row, '","') . '"' . '),';
+                    $sqlQuestion .= '("' . implode('","', $row) . '"' . '),';
                 }
             }
         }
 
         $sqlQuestion = substr($sqlQuestion, 0, -1) . ";";
 
-        $newFile = $this->dirName . '/bd/' . $this->fileName . '.sql';
+        $newFile = $this->outDirName . $this->fileName . '.sql';
         /** @var TYPE_NAME $resoursFile */
         $resoursFile = fopen($newFile, 'w');
         $result = fwrite($resoursFile, $sqlQuestion);
         fclose($resoursFile);
-
         return $result;
 
     }
