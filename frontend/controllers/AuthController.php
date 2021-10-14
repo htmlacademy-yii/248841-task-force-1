e0f24317776f974241aa02b4b6da7ce55d7c8cec
Vladimir <vova_lobochkin@mail.ru>
2021-10-14 09:53:27 +0300

    module5-task2

--- frontend/controllers/AuthController.php
+++ frontend/controllers/AuthController.php
@@ -3,12 +3,21 @@
 
 namespace frontend\controllers;
 
+use frontend\models\City;
 use frontend\models\Users;
+use rmrevin\yii\ulogin\AuthAction;
 use Yii;
 use yii\web\Controller;
 
 class AuthController extends Controller
 {
+    public function beforeAction($action)
+    {
+        if (in_array($action->id, ['ulogin-auth'])) {
+            $this->enableCsrfValidation = false;
+        }
+        return parent::beforeAction($action);
+    }
 
     public function actionIndex()
     {
@@ -26,4 +35,46 @@ class AuthController extends Controller
         return $this->render('index',['model' => $user]);
     }
 
+    public function actions()
+    {
+        return [
+            'ulogin-auth' => [
+                'class' => AuthAction::className(),
+                'successCallback' => [$this, 'uloginSuccessCallback'],
+                'errorCallback' => function ($data) {
+                    \Yii::error($data['error']);
+                },
+            ]
+        ];
+    }
+
+    public function uloginSuccessCallback($attributes = false)
+    {
+        if ($attributes) {
+            $user = Users::findOne(['email' => $attributes['email']]);
+            if ($user) {
+                \Yii::$app->user->login($user);
+            } else {
+                $user = new Users();
+                $user->email = $attributes['email'];
+                $user->name = $attributes['first_name'] . $attributes['last_name'];
+                $user->password = Yii::$app->security->generateRandomString(8);
+
+                if (strlen(trim($attributes['city'])) > 0) {
+                    $city = City::findOne(['name' => $attributes['city']]);
+
+                    if (!$city) {
+                        $newCity = new City();
+                        $newCity->name = $attributes['city'];
+                        $newCity->save();
+                        $user->city_id = $newCity->id;
+                    } else {
+                        $user->city_id = $city->id;
+                    }
+                }
+                $user->save(true);
+            }
+        }
+        return $this->goHome();
+    }
 }
--- frontend/views/auth/index.php
+++ frontend/views/auth/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use frontend\models\City;
+use rmrevin\yii\ulogin\ULogin;
 use yii\bootstrap\ActiveForm;
 use yii\bootstrap\Html;
 
@@ -25,7 +26,7 @@ use yii\bootstrap\Html;
                 'class' => 'registration__text-error',
                 'tag' => 'span'
             ]
-        ])->textInput(['placeholder' => 'example@mail.ru']) ; ?>
+        ])->textInput(['placeholder' => 'example@mail.ru']); ?>
         <?= $form->field($model, 'name', [
             'options' => [
                 'class' => 'field-container field-container--registration',
@@ -35,7 +36,7 @@ use yii\bootstrap\Html;
                 'class' => 'registration__text-error',
                 'tag' => 'span'
             ]
-        ])->textInput(['placeholder' => 'Иван Иванов']) ; ?>
+        ])->textInput(['placeholder' => 'Иван Иванов']); ?>
         <?= $form->field($model, 'city_id', [
             'options' => [
                 'class' => 'field-container field-container--registration',
@@ -55,9 +56,45 @@ use yii\bootstrap\Html;
                 'tag' => 'span'
             ]
         ])->passwordInput(['class' => 'input textarea']); ?>
+        <div class="auth-buttons">
+            <?= Html::submitButton('Cоздать аккаунт', ['class' => 'button button__registration']); ?>
+            <span>Регистрация<br>через:</span>
+            <?= ULogin::widget([
+                // widget look'n'feel
+                'display' => ULogin::D_PANEL,
 
-        <?= Html::submitButton('Cоздать аккаунт', ['class' => 'button button__registration']); ?>
+                // required fields
+                'fields' => [ULogin::F_FIRST_NAME, ULogin::F_LAST_NAME, ULogin::F_EMAIL, ULogin::F_CITY],
 
-        <?php ActiveForm::end(); ?>
+                // optional fields
+                'optional' => [ULogin::F_BDATE],
+
+                // login providers
+                'providers' => [ULogin::P_VKONTAKTE],
+
+                // login providers that are shown when user clicks on additonal providers button
+                'hidden' => [],
+
+                // where to should ULogin redirect users after successful login
+                'redirectUri' => ['auth/ulogin-auth'],
+
+                // force use https in redirect uri
+                'forceRedirectUrlScheme' => 'http',
+
+                // optional params (can be ommited)
+                // force widget language (autodetect by default)
+                'language' => ULogin::L_RU,
+
+                // providers sorting ('relevant' by default)
+                'sortProviders' => ULogin::S_RELEVANT,
+
+                // verify users' email (disabled by default)
+                'verifyEmail' => '0',
+
+                // mobile buttons style (enabled by default)
+                'mobileButtons' => '1',
+            ]); ?>
+            <?php ActiveForm::end(); ?>
+        </div>
     </div>
 </section>
